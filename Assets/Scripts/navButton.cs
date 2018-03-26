e5fc05d90ab329e19a2fc4ed7557f6f257b55027
SySTeMFa11URe <jam.morford@gmail.com>
2018-03-26 16:10:57 -0400

    Added button logic, tabs, and hiding

---
+++ Assets/Scripts/navButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navButton : MonoBehaviour {
+    public navigationManager navigationManager;
+    public int tabToActivate = 1;
+    public Sprite notActive;
+    public Sprite active;
+    private SpriteRenderer spriteRend;
+
+	// Use this for initialization
+	void Start () {
+        spriteRend = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (navigationManager.getCurrentTab() == tabToActivate)
+        {
+            spriteRend.sprite = active;
+        }
+        else
+        {
+            spriteRend.sprite = notActive;
+        }
+	}
+
+    void OnMouseDown()
+    {
+        navigationManager.setCurrentTab(tabToActivate);
+    }
+}
---
+++ Assets/Scripts/showOnTab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showOnTab : MonoBehaviour {
+    public navigationManager navigationManager;
+    public int tabToShowOn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(navigationManager.getCurrentTab() == tabToShowOn)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+	}
+}
