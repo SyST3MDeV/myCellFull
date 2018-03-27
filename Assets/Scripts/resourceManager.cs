4d9355a0390c829ebbfd7a21b20753806134e7d3
SySTeMFa11URe <jam.morford@gmail.com>
2018-03-27 17:14:39 -0400

    Added resource manager, ATP, ATPMax, and ATP counter

---
+++ Assets/Scripts/resourceManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class resourceManager : MonoBehaviour {
+    public int ATP = 1;
+    public int ATPMax = 5;
+    public Text ATPCounter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ATPCounter.text = "ATP: " + ATP.ToString() + "/" + ATPMax.ToString();
+	}
+
+    int getResource(string resourceToGet)
+    {
+        if(resourceToGet == "ATP")
+        {
+            return ATP;
+        }
+        else if(resourceToGet == "ATPMax")
+        {
+            return ATPMax;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    void setResource(string resourceToSet, int numToSetTo)
+    {
+        if (resourceToSet == "ATP")
+        {
+            ATP = numToSetTo;
+        }
+        else if (resourceToSet == "ATPMax")
+        {
+            ATPMax = numToSetTo;
+        }
+    }
+
+    void addResource(string resourceToAdd, int numToAdd)
+    {
+        if (resourceToAdd == "ATP")
+        {
+            ATP += numToAdd;
+        }
+        else if (resourceToAdd == "ATPMax")
+        {
+            ATPMax += numToAdd;
+        }
+    }
+}
