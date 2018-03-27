ca0fdec5dd277cd6ba5315ef473ee1c8b7fbe486
SySTeMFa11URe <jam.morford@gmail.com>
2018-03-27 16:48:52 -0400

    Added tab icons

---
+++ Assets/Scripts/navIcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navIcon : MonoBehaviour {
+    public int tabToSelectOn;
+    public Sprite notActive;
+    public Sprite active;
+    public navigationManager navManager;
+    private SpriteRenderer spriteRend;
+
+	// Use this for initialization
+	void Start () {
+        spriteRend = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (navManager.getCurrentTab() == tabToSelectOn)
+        {
+            spriteRend.sprite = active;
+        }
+        else
+        {
+            spriteRend.sprite = notActive;
+        }
+    }
+}
